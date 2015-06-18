34a078d53f6768d16c97126b2094bfe6ea1c646b
Dmitry Lyash <dlyash@smartling.com>
2015-06-18 16:33:32 +0300

    #12 : removed not used import

--- src/test/java/com/smartling/marketo/sdk/rest/transport/HttpCommandExecutorTest.java
+++ src/test/java/com/smartling/marketo/sdk/rest/transport/HttpCommandExecutorTest.java
@@ -10,7 +10,6 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
