b6ce662c72734f408478bc6d0347fc664783b5cc
Denis Kurochkin <dkurochkin@smartling.com>
2015-07-16 10:05:43 +0300

    MRKT-40 forgotten setters

--- src/main/java/com/smartling/marketo/sdk/FolderDetails.java
+++ src/main/java/com/smartling/marketo/sdk/FolderDetails.java
@@ -28,56 +28,112 @@ public class FolderDetails {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Date getCreatedAt() {
         return createdAt;
     }
 
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public Date getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
     public String getUrl() {
         return url;
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     public FolderId getFolderId() {
         return folderId;
+    }
+
+    public void setFolderId(FolderId folderId) {
+        this.folderId = folderId;
     }
 
     public String getFolderType() {
         return folderType;
     }
 
+    public void setFolderType(String folderType) {
+        this.folderType = folderType;
+    }
+
     public FolderId getParent() {
         return parent;
+    }
+
+    public void setParent(FolderId parent) {
+        this.parent = parent;
     }
 
     public String getPath() {
         return path;
     }
 
+    public void setPath(String path) {
+        this.path = path;
+    }
+
     public boolean isArchive() {
         return isArchive;
+    }
+
+    public void setIsArchive(boolean isArchive) {
+        this.isArchive = isArchive;
     }
 
     public boolean isSystem() {
         return isSystem;
     }
 
+    public void setIsSystem(boolean isSystem) {
+        this.isSystem = isSystem;
+    }
+
     public int getAccessZoneId() {
         return accessZoneId;
+    }
+
+    public void setAccessZoneId(int accessZoneId) {
+        this.accessZoneId = accessZoneId;
     }
 
     public String getWorkspace() {
         return workspace;
     }
 
+    public void setWorkspace(String workspace) {
+        this.workspace = workspace;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public boolean isFolderForEmails() {
