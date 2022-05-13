1afc03a4a7dfd38da6937a7932d53cb9ecd11243
pryrnjn <priya.prakash@ringcentral.com>
2022-05-13 08:34:00 +0530

    constructor

--- src/main/java/com/smartling/marketo/sdk/domain/campaign/LeadId.java
+++ src/main/java/com/smartling/marketo/sdk/domain/campaign/LeadId.java
@@ -1,11 +1,11 @@
 package com.smartling.marketo.sdk.domain.campaign;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter @ToString
+@Getter @Setter @ToString @AllArgsConstructor
 public class LeadId {
     private int id;
-
 }
