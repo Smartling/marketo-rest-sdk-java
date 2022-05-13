a24f918d6d89b44ccfc1a97a85b260b024b08163
pryrnjn <priya.prakash@ringcentral.com>
2022-05-13 17:31:39 +0530

    changes to support POST_BODY

--- src/main/java/com/smartling/marketo/sdk/rest/MarketoTriggerCampaignClient.java
+++ src/main/java/com/smartling/marketo/sdk/rest/MarketoTriggerCampaignClient.java
@@ -1,7 +1,7 @@
 package com.smartling.marketo.sdk.rest;
 
 import com.smartling.marketo.sdk.MarketoApiException;
-import com.smartling.marketo.sdk.domain.campaign.LeadId;
+import com.smartling.marketo.sdk.domain.campaign.TriggerCampaignRequest;
 import com.smartling.marketo.sdk.domain.campaign.TriggerCampaignResult;
 import com.smartling.marketo.sdk.rest.command.triggercampaign.RequestTriggerCampaign;
 
@@ -15,10 +15,18 @@ public class MarketoTriggerCampaignClient {
         this.httpCommandExecutor = httpCommandExecutor;
     }
 
-    public TriggerCampaignResult requestTriggerCampaign(int campaignId, List<LeadId> leadIds)
+    public TriggerCampaignResult requestTriggerCampaign(int campaignId,
+            TriggerCampaignRequest.LeadId[] leadIds)
+            throws MarketoApiException {
+        return this.requestTriggerCampaign(campaignId, leadIds,
+                new TriggerCampaignRequest.Token[]{});
+    }
+
+    public TriggerCampaignResult requestTriggerCampaign(int campaignId,
+            TriggerCampaignRequest.LeadId[] leadIds, TriggerCampaignRequest.Token[] tokens)
             throws MarketoApiException {
         List<TriggerCampaignResult> triggerCampaignResults = httpCommandExecutor.execute(
-                new RequestTriggerCampaign(campaignId, leadIds));
+                new RequestTriggerCampaign(campaignId, leadIds, tokens));
         return triggerCampaignResults.get(0);
     }
 }
--- src/main/java/com/smartling/marketo/sdk/rest/command/triggercampaign/RequestTriggerCampaign.java
+++ src/main/java/com/smartling/marketo/sdk/rest/command/triggercampaign/RequestTriggerCampaign.java
@@ -1,46 +1,38 @@
 package com.smartling.marketo.sdk.rest.command.triggercampaign;
 
 import com.google.common.collect.ImmutableMap;
-import com.smartling.marketo.sdk.domain.campaign.LeadId;
+import com.smartling.marketo.sdk.domain.campaign.TriggerCampaignRequest;
 import com.smartling.marketo.sdk.domain.campaign.TriggerCampaignResult;
 import com.smartling.marketo.sdk.rest.command.BaseMarketoCommand;
 
-import java.util.List;
 import java.util.Map;
 
 public class RequestTriggerCampaign extends BaseMarketoCommand<TriggerCampaignResult> {
 
     private final int campaignId;
-    private final LeadId[] leadIds;
+    private final TriggerCampaignRequest triggerCampaignRequest;
 
-    public RequestTriggerCampaign(int campaignId, List<LeadId> leadIds) {
+    public RequestTriggerCampaign(int campaignId, TriggerCampaignRequest.LeadId[] leadIds,
+            TriggerCampaignRequest.Token[] tokens) {
         super(TriggerCampaignResult.class);
         this.campaignId = campaignId;
-        this.leadIds = (LeadId[]) leadIds.toArray();
-    }
-
-    public RequestTriggerCampaign(int campaignId, LeadId[] leadIds) {
-        super(TriggerCampaignResult.class);
-        this.campaignId = campaignId;
-        this.leadIds = leadIds;
+        this.triggerCampaignRequest = new TriggerCampaignRequest(leadIds, tokens);
     }
 
     @Override
     public String getPath() {
-        return "/rest/v1/campaigns/" + campaignId + "/trigger.json";
+        return "/v1/campaigns/" + campaignId + "/trigger.json";
     }
 
     @Override
     public String getMethod() {
-        return "POST";
+        return "POST_BODY";
     }
 
     @Override
     public Map<String, Object> getParameters() {
         ImmutableMap.Builder<String, Object> builder = ImmutableMap.<String, Object>builder()
-                .put("leads", this.leadIds)
-                .put("tokens", new Object[]{});
-
+                .put("input", this.triggerCampaignRequest);
         return builder.build();
     }
 }
